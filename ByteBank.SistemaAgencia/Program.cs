782192d479731e5894017cc7c19dab63202066e5
DevPamela <pamelajusti@yahoo.com.br>
2021-10-28 20:33:42 -0300

    main

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,33 @@
+﻿using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Idades = new int[6];
+
+            Idades[0] = 15;
+            Idades[1] = 28;
+            Idades[2] = 35;
+            Idades[3] = 50;
+            Idades[4] = 28;
+
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+}
+
